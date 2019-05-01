2be86a6fc3598bd56c1f367d17c649c0c5d5b072
Flavien GAUJARD <fgaujard@e2r12p5.42.fr>
2019-05-01 18:08:32 +0200

    lasted

--- roger_skyline_deploy.sh
+++ roger_skyline_deploy.sh
@@ -13,20 +13,22 @@ if [ "$yesno" = Y ] || [ "$yesno" = y ]; then
 	usr=$(who | cut -f1 -d ' ')
 	adduser $usr sudo
 	usermod -aG sudo $usr
+	service sudo restart
+	exit
 	#-----fix ip and netmask-----
-	rm -f /etc/network/interfaces
-	mv files/interfaces /etc/network/
-	rm -f /etc/ssh/sshd_config
-	mv files/sshd_config /etc/ssh/
-	service networking restart
-	service ssh restart
-	service sshd restart
+	sudo rm -f /etc/network/interfaces
+	sudo mv files/interfaces /etc/network/
+	sudo rm -f /etc/ssh/sshd_config
+	sudo mv files/sshd_config /etc/ssh/
+	sudo service networking restart
+	sudo service ssh restart
+	sudo service sshd restart
 	#--------Public Keys---------
 	echo "You have to enter this command in your Mac OS X terminal :"
 	echo "ssh-keygen -t rsa"
 	echo "Now you have two files : << id_rsa >> and << id_rsa.pub >>"
 	echo "Send id_rsa.pub with this command with the good username :"
-	echo "ssh-copy-id -i id_rsa.pub user@10.12.15.2 -p 55555"
+	echo "ssh-copy-id -i id_rsa.pub $usr@10.12.15.2 -p 55555"
 	echo "Did you have send the public key ? write : << yes i have >> "
 	read yih
 	while [ "$yih" -ne "yes i have" ]
@@ -36,64 +38,64 @@ if [ "$yesno" = Y ] || [ "$yesno" = y ]; then
 	done
 	
 	#-----------firewall--------
-	iptables -F
-	iptables -X
-	iptables -P INPUT DROP
-	iptables -P OUTPUT DROP
-	iptables -P FORWARD DROP
-	iptables -A INPUT -p tcp -i enp0s3 --dport 55555 -j ACCEPT
-	iptables -A OUTPUT -p tcp -o enp0s3 --dport 55555 -j ACCEPT
-	iptables -A INPUT -p tcp -i enp0s3 --dport 80 -j ACCEPT
-	iptables -A OUTPUT -p tcp -o enp0s3 --dport 80 -j ACCEPT
-	iptables -A INPUT -p tcp -i enp0s3 --dport 443 -j ACCEPT
-	iptables -A OUTPUT -p tcp -o enp0s3 --dport 443 -J ACCEPT
-	iptables -I INPUT 2 -i lo -j ACCEPT
-	iptables -I OUTPUT 2 -o lo -j ACCEPT
-	iptables -A INPUT -i enp0s3 -p icmp -j ACCEPT
-	iptables -A OUTPUT -o enp0s3 -p icmp -j ACCEPT
-	iptables-save > /etc/iptables.rules
-	service networking restart
-	service ssh restart
+	sudo iptables -F
+	sudo iptables -X
+	sudo iptables -P INPUT DROP
+	sudo iptables -P OUTPUT DROP
+	sudo iptables -P FORWARD DROP
+	sudo iptables -A INPUT -p tcp -i enp0s3 --dport 55555 -j ACCEPT
+	sudo iptables -A OUTPUT -p tcp -o enp0s3 --dport 55555 -j ACCEPT
+	sudo iptables -A INPUT -p tcp -i enp0s3 --dport 80 -j ACCEPT
+	sudo iptables -A OUTPUT -p tcp -o enp0s3 --dport 80 -j ACCEPT
+	sudo iptables -A INPUT -p tcp -i enp0s3 --dport 443 -j ACCEPT
+	sudo iptables -A OUTPUT -p tcp -o enp0s3 --dport 443 -J ACCEPT
+	sudo iptables -I INPUT 2 -i lo -j ACCEPT
+	sudo iptables -I OUTPUT 2 -o lo -j ACCEPT
+	sudo iptables -A INPUT -i enp0s3 -p icmp -j ACCEPT
+	sudo iptables -A OUTPUT -o enp0s3 -p icmp -j ACCEPT
+	sudo iptables-save > /etc/iptables.rules
+	sudo service networking restart
+	sudo service ssh restart
 	#----------fail2ban----------
 	#---------portsentry---------
-	rm -f /etc/default/portsentry
-	mv files/portsentry /etc/default/
-	rm -f /portsentry/portsentry.conf
-	mv files/portsentry.conf /etc/portsentry
-	service portsentry restart
+	sudo rm -f /etc/default/portsentry
+	sudo mv files/portsentry /etc/default/
+	sudo rm -f /portsentry/portsentry.conf
+	sudo mv files/portsentry.conf /etc/portsentry
+	sudo service portsentry restart
 	#-------disable service-------
-	systemctl disable console-setup.service
-	systemctl disable keyboard-setup.service
-	systemctl disable apt-daily.timer
-	systemctl disable apt-daily-upgrade.timer
-	systemctl disable syslog.service
+	sudo systemctl disable console-setup.service
+	sudo systemctl disable keyboard-setup.service
+	sudo systemctl disable apt-daily.timer
+	sudo systemctl disable apt-daily-upgrade.timer
+	sudo systemctl disable syslog.service
 	#----------script cron--------
-	mv script /
-	rm -f /etc/crontab
-	mv files/crontab /etc/crontab
+	sudo mv script /
+	sudo rm -f /etc/crontab
+	sudo mv files/crontab /etc/crontab
 	#------------web part---------
-	rm -f /var/www/html/index.html
-	mv web/* /var/www/html
-	mkdir certs
-	cd certs
-	openssl genrsa -des3 -out server.key 1024 
-	openssl req -new -key server.key -out server.csr
-	cp server.key server.key.org
-	openssl rsa -in server.key.org -out server.key
-	openssl x509 -req -days -365 -in server.csr -signkey server.key -out server.crt
-	cd ..
-	mv certs /var/
-	rm -f /etc/apache2/sites-available/default-ssl.conf
-	mv files/default-ssl.conf /etc/apache2/sites-available/
-	rm -f /etc/apache2/sites-available/000-default.conf
-	mv files/000-default.conf /etc/apache2/sites-availables/
-	a2enmod ssl
-	a2enmod headers
-	a2ensite default-ssl
-	a2enconf ssl-params
-	systemctl reload apache2
+	sudo rm -f /var/www/html/index.html
+	sudo mv web/* /var/www/html
+	sudo mkdir certs
+	sudo cd certs
+	sudo openssl genrsa -des3 -out server.key 1024 
+	sudo openssl req -new -key server.key -out server.csr
+	sudo cp server.key server.key.org
+	sudo openssl rsa -in server.key.org -out server.key
+	sudo openssl x509 -req -days -365 -in server.csr -signkey server.key -out server.crt
+	sudo cd ..
+	sudo mv certs /var/
+	sudo rm -f /etc/apache2/sites-available/default-ssl.conf
+	sudo mv files/default-ssl.conf /etc/apache2/sites-available/
+	sudo rm -f /etc/apache2/sites-available/000-default.conf
+	sudo mv files/000-default.conf /etc/apache2/sites-availables/
+	sudo a2enmod ssl
+	sudo a2enmod headers
+	sudo a2ensite default-ssl
+	sudo a2enconf ssl-params
+	sudo systemctl reload apache2
 	#---------Reboot sys----------
-	reboot
+	sudo reboot
 elif [ "$yesno" = N ] || [ "$yesno" = n ]; then
 	echo "\033[30;42m[ Annulation by quit ]\033[0m"
 	exit 0
